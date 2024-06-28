35a3c6f0bef38959a6981dfb9ae183487a58f9f7
SamraNawaz <167023891+SamraNawaz@users.noreply.github.com>
2024-06-28 22:18:03 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Name from "./samra";
+
+const App = () => {
+  return (
+    <div>
+      <Name />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/samra.jsx
@@ -0,0 +1,14 @@
+import react from "react"
+const Name = () => {
+    return (
+      <div>
+        <h1>Hello </h1>
+        <h1>Samraa</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, explicabo?
+        </p>
+        <img src="/vite.svg" alt="" />
+      </div>
+    );
+  };
+  export default Name;
